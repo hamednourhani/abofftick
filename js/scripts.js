85526b58c4272014564b28b6dbdae6f3b8299285
Hamed Nourhani <info@itstar.ir>
2016-04-02 19:51:41 +0430

    some changes

--- js/scripts.js
+++ js/scripts.js
@@ -67,6 +67,36 @@ jQuery(document).ready(function($){
 		var main_menu = $('nav.main-menu');
 		main_menu.slideToggle();
 	});
+
+	//$(".slider").slick({
+    //
+	//	// normal options...
+	//	infinite: false,
+    //
+	//	// the magic
+	//	responsive: [{
+    //
+	//		breakpoint: 1024,
+	//		settings: {
+	//			slidesToShow: 3,
+	//			infinite: true
+	//		}
+    //
+	//	}, {
+    //
+	//		breakpoint: 600,
+	//		settings: {
+	//			slidesToShow: 2,
+	//			dots: true
+	//		}
+    //
+	//	}, {
+    //
+	//		breakpoint: 300,
+	//		settings: "unslick" // destroys slick
+    //
+	//	}]
+	//});
 	
 });	
 		
