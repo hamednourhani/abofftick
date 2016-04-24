ef0dc8b8590ae78b7aceb0237bf4b205a028c12a
Hamed Nourhani <info@itstar.ir>
2016-04-24 17:05:29 +0430

    first page

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,12 @@
 
 jQuery(document).ready(function($){
 	
-	// setTimeout(function(){ 
-	// 	window.loading_screen.finish();
-	//  }, 1000);
 
-	//$('nav.main-menu').scrollToFixed({
-	//	minWidth : '700',
-	//});
 	$('.menu-search-area span#submit').click(function(e){
 		console.log("i am clicked");
 		$('nav.main-menu form#searchform').trigger('submit');
 	});
 
-	// var smh = $('.sidebar-widget').outerHeight(true) + 40;
-	// $('.secondary').css('min-height',smh);
- 
-	// $('.sidebar-widget').scrollToFixed({
- //            limit: function() {
- //                var limit = $('.site-footer').offset().top - $('.sidebar-widget').outerHeight(true) - 40;
- //                return limit;
- //            },
-	// 		marginTop: 50,
-	// 		minWidth: 750,
- //            zIndex: 998,
- //            fixed: function() {  },
- //            dontCheckForPositionFixedSupport: true
- //        });
-
- //    $('aside,article').onScreen({
-	//    container: window,
-	//    direction: 'vertical',
-	  
-	//    doIn: function() {
-	//      // Do something to the matched elements as they come in
-	//    },
-	//    doOut: function() {
-	//     // Do something to the matched elements as they get off scren
-	//    },
-	//    tolerance: 0,
-	//    throttle: 250,
-	//    toggleClass: 'onScreen',
-	//    lazyAttr: null,
-	//    lazyPlaceholder: 'someImage.jpg',
-	//    debug: false
-	// });
-
-	// $('a#register-show').click(function(event){
-	// 	event.preventDefault();
-	// 	$('.register-container').toggleClass('form-display');
-	// 	$('.login-container').removeClass('form-display');
-	// });
-
-	// $('a#login-show').click(function(event){
-	// 	event.preventDefault();
-	// 	$('.login-container').toggleClass('form-display');
-	// 	$('.register-container').removeClass('form-display');
-	// });
-
-	// setTimeout(function(){
-	// 	window.loading_screen.finish();
-	// },2000);
 	
 	$('span#menu-toggler').click(function(){
 		console.log("clicked");
@@ -68,35 +14,12 @@ jQuery(document).ready(function($){
 		mobile_nav.slideToggle();
 	});
 
-	//$(".slider").slick({
-    //
-	//	// normal options...
-	//	infinite: false,
-    //
-	//	// the magic
-	//	responsive: [{
-    //
-	//		breakpoint: 1024,
-	//		settings: {
-	//			slidesToShow: 3,
-	//			infinite: true
-	//		}
-    //
-	//	}, {
-    //
-	//		breakpoint: 600,
-	//		settings: {
-	//			slidesToShow: 2,
-	//			dots: true
-	//		}
-    //
-	//	}, {
-    //
-	//		breakpoint: 300,
-	//		settings: "unslick" // destroys slick
-    //
-	//	}]
-	//});
+	$('#search-toggler').click(function(){
+		console.log("clicked");
+		var buttom_search_container = $('#buttom-search-container');
+		buttom_search_container.slideToggle();
+	});
+
 	$('.owl-carousel').owlCarousel({
 		margin:10,
 		//loop:true,
